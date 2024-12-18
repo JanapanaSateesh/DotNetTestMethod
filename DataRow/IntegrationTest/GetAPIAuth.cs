49b0129ce854e009a841a61ff71cf0ec950b2730
Janapana <i42318@air-worldwide.com>
2024-12-18 20:37:12 +0530

    code

---
+++ DataRow/IntegrationTest/GetAPIAuth.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataRow.IntegrationTest
+{
+    [TestClass]
+    public class GetAPIAuth
+    {
+        [TestMethod]
+        [DataRow(10,20,DisplayName ="addition")]
+        [DataRow(30,40,DisplayName ="Multiplaction")]
+        public void MyFirstTest(int a, int b) 
+        {
+            Console.WriteLine($"MyFirstTest{a}{b}");
+        }
+    }
+}
