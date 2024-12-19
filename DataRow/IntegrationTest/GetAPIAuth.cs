621caf6a207f4b7a0f32ba2cdcafa0423a733210
Janapana <i42318@air-worldwide.com>
2024-12-19 10:28:29 +0530

    Test1

--- DataRow/IntegrationTest/GetAPIAuth.cs
+++ DataRow/IntegrationTest/GetAPIAuth.cs
@@ -16,7 +16,7 @@ namespace DataRow.IntegrationTest
         public void MyFirstTest(int a, int b) 
         {
             Console.WriteLine($"MyFirstTest{a}{b}");
-            //Test
+            //Test1
         }
     }
 }
